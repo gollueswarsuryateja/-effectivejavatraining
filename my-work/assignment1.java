0966ae4713ee14c929b56c7e78d61fc8631bf56d
gollueswarsuryateja <gollueswar2@gmail.com>
2021-10-26 09:45:15 +0530

    Day 1 assignment

---
+++ my-work/assignment1.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+import account.BankAccount;
+
+class assignment1{
+	public static void main(String args[]){
+		
+		Scanner read = new Scanner(System.in);
+		BankAccount ba = new BankAccount("0124689106438","eswarsuryateja","12345678",1000,0.2);
+		
+		while(true){
+			System.out.println("1. Deposit\n2. Withdrawn\n3. Credit Interest\n0. Exit\n4. Options\nEnter your choice");
+			switch(read.nextInt()){
+				case 1:
+					ba.depositAmount();
+					break;
+				case 2:
+					ba.withdrawAmount();
+					break;
+				case 3:
+					ba.creditInterest();
+					break;
+				case 4:
+					ba.options();
+					break;
+				case 0:
+					return;
+			}
+		}
+	}
+}
