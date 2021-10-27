88ea5d9f48871cf44018211b9241adc8253f74dd
gollueswarsuryateja <89844767+gollueswarsuryateja@users.noreply.github.com>
2021-10-27 08:32:46 +0530

    day2 assignment

--- my-work/BankAccount.java
+++ my-work/BankAccount.java
@@ -1,122 +1,136 @@
-package account;
-public class BankAccount{
-	String acc_no,name,password;
-	float balance, interest_rate;
-	Scanner get = new Scanner(System.in);
-	
-		public BankAccount()
-		{
-		}
-		public BankAccount(String accno, String acc_name, String pass, float amount, double rate){
-			acc_no = accno;
-			name = acc_name;
-			password = pass;
-			balance = amount;
-			interest_rate = (float) rate;
-		}
+package bankstructure;
+import java.util.Scanner;
+
+	public class BankAccount {
 		
-		public void creditInterest(){
-			balance = balance + (balance * interest_rate / 1200);
-		}
+		private int accountNumber;
+		private String name;
+		private String password;
+		private double balance;
+		private double interestRate;
 		
-		public void depositAmount(){
-			System.out.println("Enter Deposit Amount : ");
-			float amount = get.nextFloat();
-			if (amount > 0){
-				balance = balance + amount;
-				System.out.println(amount + "Deposited to the Account " + acc_no);
-				System.out.println("Show balance (y/n)");
-				if(get.next().charAt(0) == 'y'){ displayBalance(); }
-			}
-			else{
-				System.out.println("Deposite Failed..!");
-			}
-		}
-		
-		public void withdrawAmount(){
-			System.out.println("Enter Withdrawal Amount : ");
-			float amount = get.nextFloat();
-			System.out.println("Enter password : ");
-			String pass = get.nextLine();
-			if(amount > 0 && amount < balance){
-				if(password.equals(pass)){
-					balance = balance - amount;
-					System.out.println(amount + "Debited from Account " + acc_no);
-					System.out.println("Show balance (y/n)");
-					if(get.next().charAt(0) == 'y'){ displayBalance(); }
-				}
-				else{
-					System.out.println("Wrong Password Entered..!");
-				}
-			}
-			else{
-				System.out.println("Amount can not be distributed..!");
-			}
-		}
-		
-		public void options(){
-			System.out.println("1.Account Number\n2. Interest Rate\n3.Account Holder Name\n4. Display Balance\n5.Change Password\n0. exit");
-			switch(get.nextInt())
-			{
-				case 1:
-					viewAccNumber();
-					break;
-				case 2:
-					interestFunc();
-					break;
-				case 3:
-					accountName();
-					break;
-				case 4:
-					displayBalance();
-					break;
-				case 5:
-					changePassword();
-					break;
-				case 0:
-					return;
-			}
-		}
-		
-		public void viewAccNumber(){
-			System.out.println("Account Number : " + acc_no);
-		}
-		
-		public void interestFunc(){
-			System.out.println("Interest Rate for the \naccount : " + acc_no + "\nInterest Rate : " + interest_rate);
-			System.out.print("Do you want change Interest Rate (y/n) : ");
-			if(get.next().charAt(0) == 'y'){
-				System.out.print("Enter new Interest Rate : ");
-				interest_rate = get.nextFloat();
-			}
+		Scanner scan=new Scanner(System.in);
+		public int getAccountNumber() {
+			return accountNumber;
 		}
 
-		public void accountName(){
-			System.out.println("Account Holder Name : " + name);
-			System.out.print("Do you want change Account Holder Name (y/n) : ");
-			if(get.next().charAt(0) == 'y'){
-				System.out.print("Enter Name : ");
-				name = get.nextLine();
+
+		public String getName() {
+			return name;
+		}
+
+		public void setName(String name) {
+			this.name = name;
+		}
+
+		
+		public double getBalance() {
+			System.out.println(balance);
+			return balance;
+		}
+
+		
+		public double getInterestRate() {
+			return interestRate;
+		}
+
+		public void setInterestRate(double interestRate) {
+			this.interestRate = interestRate;
+		}
+		
+
+		
+		
+		public String getPassword() {
+			return password;
+		}
+
+		public void setPassword(String password) {
+			
+			this.password = salt(password);
+		}
+
+		
+
+		private String salt(String password) {
+			
+			String salted="";
+			
+			for(int i=0;i<password.length();i++) {
+				int ch= (int) password.charAt(i)+2;
+				salted+=Integer.toBinaryString(ch);
 			}
+			return salted;
+		}
+
+		public BankAccount(int accountNumber, String name, String password, double amount,double interestRate) {
+			
+			balance=amount; //this is optional here as there is a single balance in the context	
+			this.name=name;
+			setPassword(password);
+			this.interestRate=interestRate;
+			this.accountNumber=accountNumber;
+			
 		}
 		
-		public void displayBalance(){
-			System.out.println("Current Balance is : " + balance);
+		public void show() {
+			System.out.println("account number\t"+accountNumber);
+			System.out.println("name         \t"+name);
+			System.out.println("password     \t"+password);
+			System.out.println("balance      \t"+balance);
+			System.out.println("interest rate\t"+interestRate);
+			System.out.println();
+			
 		}
+
+		public void deposit() {
+			
+			System.out.println("Enter the amount to deposit :");
+			long Amount=scan.nextLong();
+			balance=balance+Amount;
+			System.out.println("Successfully deposited now your balance is" +(balance)+" rupees");
+			
+			}
 		
-		public void changePassword(){
-			System.out.println("Do you want to change password (y/n)");
-			String pass;
-			if(get.next().charAt(0) == 'y'){
-				System.out.print("Enter old Password : ");
-				if(get.nextLine() == password){
-					System.out.print("Enter new password : ");
-					pass = get.nextLine();
-					System.out.print("Retype password : ");
-					if(pass == get.nextLine()){
-						password = pass;
-					}
+
+		public void withdraw() {
+			String psw;
+			
+			System.out.println("Enter the password :");
+			psw=scan.next();
+			System.out.println(password);
+			if(password.equals(psw))
+			{
+				System.out.println("Enter the amount to be withdraw :");
+				long Amount=scan.nextLong();
+				if(Amount<=0)
+				{
+					System.out.println("Should not withdraw amount.Please withdraw some other Amount!!!");
 				}
+				else if(Amount>balance)
+				{
+					System.out.println("Less Balance..Please withdraw some other Amount!!!");
+				}
+				else 
+				{
+					System.out.println("Successfully withdraw!! Available balance is " +(balance-Amount)+" rupees");
+				}
+				
 			}
+			else
+			{
+				System.out.println("Invalid Password!!!!");
+			}
+			
+			
 		}
+		
+		public void creditInterest() {
+			balance+=(balance*interestRate/1200);
+                        System.out.println("Credited Interest is :"+Balance);
+		}
+		
+
+
+	
 }
