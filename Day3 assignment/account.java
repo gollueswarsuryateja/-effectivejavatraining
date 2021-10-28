edd9d1642266ebd9d115995b8a1c78c08e8ed481
gollueswarsuryateja <89844767+gollueswarsuryateja@users.noreply.github.com>
2021-10-28 07:07:11 +0530

    Day 3 assignment

---
+++ Day3 assignment/account.java
@@ -0,0 +1,12 @@
+package in.conceptarchitect.finance;
+
+public class account{
+
+	public static void main(String[] args) {
+		SavingsAccount a1=new SavingsAccount();
+		a1.createaccount();
+		// TODO Auto-generated method stub
+
+	}
+
+}
